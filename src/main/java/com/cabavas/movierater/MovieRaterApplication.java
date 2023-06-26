7a249c6c33084719959f6a440a2273e75118f737
Cabavas <cabavas@gmail.com>
2023-06-26 16:15:24 -0300

    Project created

---
+++ src/main/java/com/cabavas/movierater/MovieRaterApplication.java
@@ -0,0 +1,13 @@
+package com.cabavas.movierater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRaterApplication.class, args);
+	}
+
+}
